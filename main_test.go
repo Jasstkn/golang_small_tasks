f616202d36a7e11b8d46902e279ef116c7e04a22
Jasstkn <jasssstkn@yahoo.com>
2021-02-17 21:06:23 +0300

    Add tests

---
+++ main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func TestReverseArray(t *testing.T) {
+	expected := []int{3, 2, 1}
+	result := reverseArray([]int{1, 2, 3})
+
+	for i := range result {
+        if result[i] != expected[i] {
+            t.Errorf("Reverse operation isn't right, got: %v, want: %v.", result, expected)
+        }
+    }
+}
